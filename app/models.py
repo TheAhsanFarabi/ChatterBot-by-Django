cb1fc0ae76c193bbac58affe17014065a5aaa516
Ahsan Farabi <ahsanfarabi1971@gmail.com>
2024-05-25 14:45:22 +0600

    Update

--- app/models.py
+++ app/models.py
@@ -1,6 +1,11 @@
+# app/models.py
+
 from django.db import models
 
 class Chat(models.Model):
-    user_message = models.CharField(max_length=255)
-    bot_response = models.CharField(max_length=255)
+    user_message = models.TextField()
+    bot_response = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"User: {self.user_message} - Bot: {self.bot_response}"
--- app/views.py
+++ app/views.py
@@ -1,7 +1,28 @@
 # app/views.py
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
+from .models import Chat
 
 def index(request):
-    return HttpResponse("Hello, welcome to the chatterBot app!")
+    if request.method == "POST":
+        user_input = request.POST.get('user_input', '').strip().lower()
+
+        if user_input == 'hello':
+            response = "Hi there! How can I help you today?"
+        elif user_input == 'how are you?':
+            response = "I'm just a bot, but I'm functioning as expected!"
+        elif user_input == 'bye':
+            response = "Goodbye! Have a great day!"
+        else:
+            response = "Sorry, I didn't understand that."
+
+        # Save the chat to the database
+        chat = Chat(user_message=user_input, bot_response=response)
+        chat.save()
+
+        return JsonResponse({'response': response})
+
+    # Retrieve the last 10 chats to display
+    chats = Chat.objects.all().order_by('-timestamp')[:10]
+    return render(request, 'chat.html', {'chats': chats})
